b4d94d311b55115522163f940b75ba2baed0a72a
Rehab <rehab123abdallah@gmail.com>
2024-10-11 12:30:54 +0300

    Session01(GetAll&GetById)

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories;
+
+namespace Talabat.APIS.Controllers
+{
+
+    public class ProductsController : APIBaseController
+    {
+        private readonly IGenericRepository<Product> _productRepo;
+
+        public ProductsController(IGenericRepository<Product> productRepo)
+        {
+            _productRepo = productRepo;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+
+        {
+            var Products = await _productRepo.GetAllAsync();
+            //OkObjectResult result = new OkObjectResult(Products);
+            return Ok(Products);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(int Id)
+        {
+            var product = await _productRepo.GetByIdAsync(Id);
+            return Ok(product);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories;
+using Talabat.Repository;
+using Talabat.Repository.Data;
+
 namespace Talabat.APIS
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task  Main(string[] args)
         {
             Console.WriteLine("Hello Div");
             Console.WriteLine("Hello Session01");
             var builder = WebApplication.CreateBuilder(args);
-
+            #region Configure Services
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<APIDbContext>(optios=>
+            {
+                optios.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
+            });
+           // builder.Services.AddScoped<IGenericRepository<Product>,GenericRepository<Product>>();
+           builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            #endregion
+          
+
+            
             var app = builder.Build();
 
+
+            #region UpdateDatabase
+
+           using var Scope = app.Services.CreateScope();
+            var Services = Scope.ServiceProvider;
+            var LoggerFactory= Services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+                var DbContext = Services.GetRequiredService<APIDbContext>();
+                await DbContext.Database.MigrateAsync();
+                await DbContextSeed.SeedAsync(DbContext);
+            }
+            catch (Exception ex) {
+                var Logger = LoggerFactory.CreateLogger<Program>();
+                Logger.LogError(ex, "An Error Occure During Applying Migration");
+            }
+           
+
+            #endregion
+
+
+            #region Kestrol Piplines
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -30,6 +68,9 @@ namespace Talabat.APIS
 
 
             app.MapControllers();
+
+            #endregion
+
 
             app.Run();
         }
